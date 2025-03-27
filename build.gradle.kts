f2fd7abcbd74ecff268ea3d8b016eb1ec8176154
pauldennis2 <pauldennis.misc@gmail.com>
2025-03-27 18:24:12 -0400

    Added slf4j logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.36") // Use the latest version
     testImplementation("org.projectlombok:lombok:1.18.36")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("ch.qos.logback:logback-classic:1.5.3")
 }
 
 application {
--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -1,3 +1,6 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class GameRunner {
 
     public static void main(String[] args) {
@@ -5,5 +8,10 @@ public class GameRunner {
         Hero me = new Hero(50);
         me.takeDamage(10);
         System.out.println(me);
+
+        log.info("Adaptive Ascension Java Version started.");
+        log.debug("Debug message example");
+        log.warn("Warning message example");
+        log.error("Error message example");
     }
 }
